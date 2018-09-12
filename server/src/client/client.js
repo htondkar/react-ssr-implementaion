6564e07a18ccc1e4dac5139b3d48c9421366f03d
Hamidreza <htondkar@gmail.com>
2018-09-12 21:01:43 +0430

    set up ssr

---
+++ server/src/client/client.js
@@ -0,0 +1,7 @@
+import Home from './components/Home'
+import ReactDOM from 'react-dom'
+import React from 'react'
+
+ReactDOM.hydrate(<Home />, document.getElementById('root'))
+
+export default Home
---
+++ server/src/client/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => (
+  <main>
+    <h3>Home page</h3>
+    <h5>Hello world </h5>
+    <button onClick={_ => console.log('works!')}>Click me</button>
+  </main>
+)
+
+export default Home
